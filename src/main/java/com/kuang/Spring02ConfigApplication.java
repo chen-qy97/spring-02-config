12cde934fb934a5924d7dbceddbdb28fdfa71228
chenmin <chenmin@bonc.com.cn>
2021-04-15 18:21:29 +0800

    配置文件

--- src/main/java/com/kuang/Spring02ConfigApplication.java
+++ src/main/java/com/kuang/Spring02ConfigApplication.java
@@ -3,6 +3,15 @@ package com.kuang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 配置文件优先级依此为：
+ *  file:./config/     文件路径为当前项目根目录下
+ *  file:./
+ *  classpath: /config/
+ *  classpath: /   默认为当前等级
+ *
+ *  .properties 配置文件优先级高于  .yml
+ */
 @SpringBootApplication
 public class Spring02ConfigApplication {
 
